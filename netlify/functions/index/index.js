9352300ae27c82b84d740c4aeae3690d8a742f5d
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-08-06 16:09:51 +0100

    refactor

--- netlify/functions/index/index.js
+++ netlify/functions/index/index.js
@@ -1,45 +1,51 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-// const handler = async (event) => {
-//   try {
-//     const subject = event.queryStringParameters.name || "World";
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify({
-//         message: `Hello ${subject}, you're coming from this path: ${event.path}`,
-//       }),
-//       // // more keys you can return:
-//       // headers: { "headerName": "headerValue", ... },
-//       // isBase64Encoded: true,
-//     };
-//   } catch (error) {
-//     return { statusCode: 500, body: error.toString() };
-//   }
-// };
+const handler = async (event) => {
+  try {
+    if (event.path.startsWith("/sh")) {
+      return {
+        statusCode: 200,
+        body: `I'm coming from sh: ${event.path}`,
+      };
+    }
 
-// module.exports = { handler };
-
-module.exports = async (request) => {
-  const { pathname } = new URL(request.url);
-
-  if (
-    pathname.startsWith("/sh") ||
-    pathname.startsWith("/create-tauri-app.sh")
-  ) {
-    return new Response("sh", {
-      headers: { "Content-Type": "text/plain" },
-    });
+    if (event.path.startsWith("/ps")) {
+      return {
+        statusCode: 200,
+        body: `I'm coming from ps: ${event.path}`,
+      };
+    }
+  } catch (error) {
+    return {
+      statusCode: 200,
+      body: `I'm coming from anywhere else: ${event.path}`,
+    };
   }
-
-  if (
-    pathname.startsWith("/ps") ||
-    pathname.startsWith("/create-tauri-app.ps1")
-  ) {
-    return new Response("ps", {
-      headers: { "Content-Type": "text/plain" },
-    });
-  }
-
-  return new Response("catch all", {
-    headers: { "Content-Type": "text/html" },
-  });
 };
+
+module.exports = { handler };
+
+// module.exports = async (request) => {
+//   const { pathname } = new URL(request.url);
+
+//   if (
+//     pathname.startsWith("/sh") ||
+//     pathname.startsWith("/create-tauri-app.sh")
+//   ) {
+//     return new Response("sh", {
+//       headers: { "Content-Type": "text/plain" },
+//     });
+//   }
+
+//   if (
+//     pathname.startsWith("/ps") ||
+//     pathname.startsWith("/create-tauri-app.ps1")
+//   ) {
+//     return new Response("ps", {
+//       headers: { "Content-Type": "text/plain" },
+//     });
+//   }
+
+//   return new Response("catch all", {
+//     headers: { "Content-Type": "text/html" },
+//   });
+// };
