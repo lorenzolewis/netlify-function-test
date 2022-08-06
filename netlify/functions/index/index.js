7521055362e267763b1931cfa37926f334e49846
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-08-06 16:04:22 +0100

    revise function

--- netlify/functions/index/index.js
+++ netlify/functions/index/index.js
@@ -1,27 +1,45 @@
-export default {
-  async fetch(request) {
-    const { pathname } = new URL(request.url);
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+// const handler = async (event) => {
+//   try {
+//     const subject = event.queryStringParameters.name || "World";
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({
+//         message: `Hello ${subject}, you're coming from this path: ${event.path}`,
+//       }),
+//       // // more keys you can return:
+//       // headers: { "headerName": "headerValue", ... },
+//       // isBase64Encoded: true,
+//     };
+//   } catch (error) {
+//     return { statusCode: 500, body: error.toString() };
+//   }
+// };
 
-    if (
-      pathname.startsWith("/sh") ||
-      pathname.startsWith("/create-tauri-app.sh")
-    ) {
-      return new Response("sh", {
-        headers: { "Content-Type": "text/plain" },
-      });
-    }
+// module.exports = { handler };
 
-    if (
-      pathname.startsWith("/ps") ||
-      pathname.startsWith("/create-tauri-app.ps1")
-    ) {
-      return new Response("ps", {
-        headers: { "Content-Type": "text/plain" },
-      });
-    }
+module.exports = async (request) => {
+  const { pathname } = new URL(request.url);
 
-    return new Response("catch all", {
-      headers: { "Content-Type": "text/html" },
+  if (
+    pathname.startsWith("/sh") ||
+    pathname.startsWith("/create-tauri-app.sh")
+  ) {
+    return new Response("sh", {
+      headers: { "Content-Type": "text/plain" },
     });
-  },
+  }
+
+  if (
+    pathname.startsWith("/ps") ||
+    pathname.startsWith("/create-tauri-app.ps1")
+  ) {
+    return new Response("ps", {
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+
+  return new Response("catch all", {
+    headers: { "Content-Type": "text/html" },
+  });
 };
